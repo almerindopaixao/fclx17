05954224fae1036f73578cde4eb1202ed634140f
almerindopaixao <almerindo_paixao@live.com>
2024-02-13 10:35:25 -0300

    refactor: alter order api port from 3000 to 3001

--- order/src/main.ts
+++ order/src/main.ts
@@ -6,5 +6,5 @@ import { ValidationPipe } from '@nestjs/common';
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(3001);
 })();
